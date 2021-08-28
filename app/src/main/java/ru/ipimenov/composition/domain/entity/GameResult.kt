7847f86f1bf5cfb7ac3064faa64e5e8a65fe0021
MAVERICK <bravo1@list.ru>
2021-08-28 16:59:12 +0300

    Реализована навигация через FragmentManager

--- app/src/main/java/ru/ipimenov/composition/domain/entity/GameResult.kt
+++ app/src/main/java/ru/ipimenov/composition/domain/entity/GameResult.kt
@@ -1,8 +1,10 @@
 package ru.ipimenov.composition.domain.entity
 
+import java.io.Serializable
+
 data class GameResult(
     val winner: Boolean,
     val countOfRightAnswer: Int,
     val countOfQuestions: Int,
     val gameSettings: GameSettings
-)
+) : Serializable
